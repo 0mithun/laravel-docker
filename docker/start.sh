4d413527436f5356df18536646df878a22b0b202
Mithun Halder <mithunrptc@gmail.com>
2021-12-17 12:57:53 +0600

    Schedule & queue service added

--- docker/start.sh
+++ docker/start.sh
@@ -2,14 +2,27 @@
 
 set -e
 
-echo "Test echo"
-
 env=${APP_ENV:-production}
+role=${CONTAINER_ROLE:-app}
 
 if [ "$env" != "local" ]; then
     echo "Removing XDebug"
     rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
 fi
 
+echo "The role is $role..."
 
-exec apache2-foreground
+if [ "$role" = "app" ]; then
+    exec apache2-foreground
+elif [ "$role" = "scheduler" ]; then
+   while [ true ]
+   do
+    php /var/www/html/artisan schedule:run --verbose --no-interaction & sleep 60
+   done
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue:work"
+     php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90
+else 
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
